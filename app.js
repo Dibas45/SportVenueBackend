ffb26af68afe55911d42c2da1796b81fb3adb330
Dibas45 <Dibaspoudel45@gmail.com>
2025-06-07 22:34:20 +0545

    fix cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(cors(
   {
-    origin:  'http://localhost:8000', 
+    origin:  "https://sports-venue-booking-alpha.vercel.app/ ",
     credentials: true, // Allow cookies to be sent with requests
   }
 ));
