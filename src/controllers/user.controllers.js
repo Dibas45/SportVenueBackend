3fc6f6aabc7e0c94551405232db9cc2383bb8d69
Dibas45 <Dibaspoudel45@gmail.com>
2025-06-08 14:37:14 +0545

    logout fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -103,24 +103,29 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser=asyncHandler(async(req,res)=>{
-    await User.findByIdAndUpdate(req.user._id,{
-        $unset: {
-            refreshToken: 1
-        }
-    },{
-        new:true
-    } )
-    const options = {
-        httpOnly: true,
-        secure:true,
-       
-    };
-    return res.status(200).clearCookie("accessToken",  options)
-        .clearCookie("refreshToken", options)
-        .json(new ApiResponse(200, "User logged out successfully"));
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: { refreshToken: 1 },
+    },
+    { new: true }
+  );
 
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Strict", // Added for CSRF protection
+    // Consider adding 'path' if your cookies are scoped to a particular path
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, "User logged out successfully"));
 });
+
 const getUserById = async (req, res) => {
   const { id } = req.params;
 
